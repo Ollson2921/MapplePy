54776a2fcd86a2ac28bcdccfdc5f5b74ca390fc4
ReedActon <110858904+ReedActon@users.noreply.github.com>
2025-03-25 12:53:12 +0000

    Row Col Separation Attempt

--- src/testing_row_col_seperation.py
+++ src/testing_row_col_seperation.py
@@ -3,20 +3,17 @@ from gridded_cayley_permutations import Tiling, GriddedCayleyPerm
 from cayley_permutations import CayleyPermutation
 from gridded_cayley_permutations.row_col_map import RowColMap
 from tilescope_folder.strategies.row_column_separation import LessThanRowColSeparation, LessThanOrEqualRowColSeparation
+from mapplings.MT_row_col_separation import MTRowColSeperation
 
-
-P = Parameter(Tiling([],[],(2,2)), RowColMap({0:0,1:0},{0:0,1:1}))
+Base = Tiling.from_vincular(CayleyPermutation((2,1,0)),[0])
+print(Base)
+P = Parameter(Base, RowColMap({0:0,1:0,2:0,3:0,4:0,5:0,6:0},{0:0,1:0,2:0,3:1,4:1,5:1,6:1}))
 
 Q = Tiling([GriddedCayleyPerm(CayleyPermutation((0,1)),[(0,0),(0,1)])],[],(1,2))
+Q2 = Parameter(Tiling([],[],(2,2)),RowColMap({0:0,1:0},{0:0,1:1})).back_map_obs_and_reqs(Q).ghost
 
-P = P.back_map_obs_and_reqs(Q)
-print(Q)
-print(P)
-Test = LessThanOrEqualRowColSeparation(P.ghost)
-for k in Test.row_col_separation():
-    for z in LessThanOrEqualRowColSeparation(k).row_col_separation():
-        print(z)
+M = MappedTiling(Q,[P],[],[])
 
-# for k in Test.inequalities_sets():
-#     print(k)
-print(Test.row_col_map.preimage_map()) # this can get us the rows and cols that need to be seperated in the parameter
+Test = MTRowColSeperation(M)
+for T in Test.separate_base_tiling():
+    print(T)
