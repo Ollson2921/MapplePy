558463669aa06a21c0b867269b31d6ca7e9742a0
Abigail Ollson <145692399+Ollson2921@users.noreply.github.com>
2025-04-17 15:16:26 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mapped_tilings",
+    version="1.0",
+    description="A module for using Cayley permutations.",
+    author="Reid Acton, Christian Bean, and Abigail Ollson",
+    author_email="c.n.bean@keele.ac.uk",
+    packages=["mapplings"],
+    install_requires=["comb_spec_searcher"],  # external packages as dependencies
+)
