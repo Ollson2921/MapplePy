6ec01204e402fe6c92e46841b411e4aedf1b51f2
ReedActon <110858904+ReedActon@users.noreply.github.com>
2025-05-21 23:08:27 +0000

    Update factor.py

--- mapplings/algorithms/factor.py
+++ mapplings/algorithms/factor.py
@@ -1,7 +1,6 @@
 """Contains the Factor class"""
 
-from typing import Iterable
-from itertools import chain
+from itertools import chain, combinations
 from gridded_cayley_permutations.factors import Factors
 
 from mapplings import MappedTiling, ParameterList, MTCleaner
@@ -10,21 +9,15 @@ from mapplings import MappedTiling, ParameterList, MTCleaner
 Cell = tuple[int, int]
 
 
-class Factor:
+class Factor(Factors):
     """Class containing algorithms for factoring"""
 
     def __init__(self, mappling: MappedTiling):
         self.mappling = mappling
+        super().__init__(mappling.tiling)
 
-    def is_factorable(self) -> bool:
-        """Checks if the mappling is factorable"""
-        return len(self.find_factor_cells()) > 1
-
-    def find_factor_cells(self) -> tuple[tuple[Cell, ...], ...]:
-        """Returns the cells for each factor as a tuple"""
-        base_factors = set(
-            map(frozenset, Factors(self.mappling.tiling).find_factors_as_cells())
-        )
+    def get_parameter_regions(self) -> set[frozenset[Cell]]:
+        """Gets the image regions of all avoiders and all c and e lists."""
         avoider_regions = set(
             frozenset(param.image_cells())
             for param in self.mappling.avoiding_parameters
@@ -37,16 +30,24 @@ class Factor:
             frozenset(param_list.combined_image_cells())
             for param_list in self.mappling.enumerating_parameters
         )
-        all_regions = set(
-            chain(base_factors, avoider_regions, container_regions, enumerator_regions)
-        )
-        return Factor.combine_cell_groups(list(all_regions))
+        return set(chain(avoider_regions, container_regions, enumerator_regions))
 
-    def make_factors(
-        self, factor_cells: tuple[tuple[Cell, ...], ...]
-    ) -> Iterable[MappedTiling]:
+    def combine_cells_from_parameters(self) -> None:
+        """Uses the parameter regions to combine cells."""
+        all_regions = self.get_parameter_regions()
+        for cell1, cell2 in combinations(self.cells, 2):
+            for region in all_regions:
+                if cell1 in region and cell2 in region:
+                    self.combine_cells(cell1, cell2)
+                    break
+
+    def find_factors_as_cells(self):
+        self.combine_cells_from_parameters()
+        return super().find_factors_as_cells()
+
+    def find_factors(self):
         """Creates a new mappling for each factor"""
-        for factor in factor_cells:
+        for factor in self.find_factors_as_cells():
             _factor = set(factor)
 
             new_avoiders = ParameterList(
@@ -71,18 +72,3 @@ class Factor:
                 new_enumerators,
             )
             yield MTCleaner.remove_empty_rows_and_cols(new_mappling)
-
-    @staticmethod
-    def combine_cell_groups(
-        cell_groups: list[frozenset[Cell]],
-    ) -> tuple[tuple[Cell, ...], ...]:
-        """Joins sets of cells that have a shared intersection"""
-        n = len(cell_groups)
-        new_cell_groups = cell_groups
-        for i in range(n):
-            for j in range(n):
-                if new_cell_groups[i] & new_cell_groups[j]:
-                    combined = new_cell_groups[i] | new_cell_groups[j]
-                    new_cell_groups[i] = combined
-                    new_cell_groups[j] = combined
-        return tuple(map(tuple, set(new_cell_groups)))
