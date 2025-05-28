2dc9ad6ad3a7646d8217fd6bfdae8275f6328a50
Abigail Ollson <145692399+Ollson2921@users.noreply.github.com>
2025-05-28 13:27:38 +0100

    row col sep

--- mapplings/strategies/row_col_sep_mt.py
+++ mapplings/strategies/row_col_sep_mt.py
@@ -1,84 +1,353 @@
 """Module for row and column separating mapped tilings and related strategies."""
 
-from itertools import accumulate
-from typing import Iterator
 from gridded_cayley_permutations import Tiling, GriddedCayleyPerm
 from gridded_cayley_permutations.row_col_map import RowColMap
 from tilescope.strategies.row_column_separation import (
     LessThanRowColSeparation,
     LessThanOrEqualRowColSeparation,
 )
-from ..mapped_tiling import MappedTiling, Parameter, ParameterList
+from cayley_permutations import CayleyPermutation
+from ..mapped_tiling import MappedTiling, Parameter
 
 
 class LTRowColSeparationMT:
-    """
-    When separating, cells must be strictly above/below each other.
-    """
+    def __init__(self, mapped_tiling: MappedTiling, or_equal=False):
+        self.tiling = mapped_tiling.tiling
+        self.avoiding_parameters = mapped_tiling.avoiding_parameters
+        self.containing_parameters = mapped_tiling.containing_parameters
+        self.enumeration_parameters = mapped_tiling.enumerating_parameters
+        if or_equal:
+            self.separation = LessThanOrEqualRowColSeparation(self.tiling)
+        else:
+            self.separation = LessThanRowColSeparation(self.tiling)
+        self.preimage_map = self.separation.row_col_map.preimage_map()
 
-    def __init__(self, mapped_tiling: MappedTiling):
-        self.mt = mapped_tiling
-        self.separation = LessThanRowColSeparation(self.mt.tiling)
-        self.expansion_map = self.separation.row_col_map
-        self.row_expansion, self.col_expansion = self.expansions(
-            self.expansion_map.row_map
-        ), self.expansions(self.expansion_map.col_map)
+    def base_extensions(self) -> tuple:
+        """Returns how many new rows/cols are added to the new base tiling
+        for each row/col in the original tiling"""
+        row_extensions = [
+            (len(self.preimage_map[1][i]) - 1) for i in range(len(self.preimage_map[1]))
+        ]
+        col_extensions = [
+            (len(self.preimage_map[0][i]) - 1) for i in range(len(self.preimage_map[0]))
+        ]
+        return col_extensions, row_extensions
 
-        self.row_expansion, self.col_expansion = {0: 3, 1: 2}, {
-            0: 3,
-            1: 2,
-        }  # TODO: remove this!!
+    def parameter_extensions(self, base_extensions, param: Parameter):
+        """Returns how many rows/cols must be added to the new parameter
+        for each row/col in the original tiling"""
+        col_parameter_extensions = [
+            base_extensions[0][i] * len(param.map.preimages_of_col(i))
+            for i in range(len(base_extensions[0]))
+        ]
+        row_parameter_extensions = [
+            base_extensions[1][i] * len(param.map.preimages_of_row(i))
+            for i in range(len(base_extensions[1]))
+        ]
+        return col_parameter_extensions, row_parameter_extensions
 
-    def expansions(self, rowcol_map: dict[int]) -> dict[int]:
-        """Returns a list of multipliers for how much larger each row/column
-        becomes in the map."""
-        expansion = {}
-        for val in set(rowcol_map.values()):
-            expansion[val] = sum(1 for v in rowcol_map.values() if v == val)
-        return expansion
+    def total_extensions(self, parameter_extensions):
+        """Returns how far each preimage group moves in the new parameter"""
+        total_extensions = (
+            [0]
+            + [
+                sum(parameter_extensions[0][: k + 1])
+                for k in range(len(parameter_extensions[0]))
+            ],
+            [0]
+            + [
+                sum(parameter_extensions[1][: k + 1])
+                for k in range(len(parameter_extensions[1]))
+            ],
+        )
+        return total_extensions
 
-    def map_param(self, param: Parameter) -> Parameter:
-        """Maps a parameter to a new parameter."""
-        param_row_expansion = self.expansions(param.row_map)
-        param_col_expansion = self.expansions(param.col_map)
-        new_row_expan = {
-            key: param_row_expansion[key] * self.row_expansion[key]
-            for key in param_row_expansion
-        }
-        new_col_expan = {
-            key: param_col_expansion[key] * self.col_expansion[key]
-            for key in param_col_expansion
-        }
-        row_partial_sum = list(accumulate(new_row_expan.values()))
-        col_partial_sum = list(accumulate(new_col_expan.values()))
-        new_row_map = self.new_row_col_map(row_partial_sum)
-        new_col_map = self.new_row_col_map(col_partial_sum)
-        new_map = RowColMap(new_row_map, new_col_map)
+    def cell_map(self, total_extensions, param: Parameter):
+        """Maps each cell of the original parameter to the correct active cell in the original parameter
+        also tracks which cells will be empty"""
+        cell_map = {}
+        for i in range(param.ghost.dimensions[0]):
+            for j in range(param.ghost.dimensions[1]):
+                cell_map[(i, j)] = (
+                    i + total_extensions[0][param.map.col_map[i]],
+                    j + total_extensions[1][param.map.row_map[j]],
+                )
+        inequalities = self.separation.column_row_inequalities()
+        cells_to_move = set([pair[1] for pair in inequalities[0]])
+        for base_cell in cells_to_move:
+            if base_cell in param.image_cells():
+                for cell in param.map.preimage_of_cell(base_cell):
+                    cell_map[cell] = (
+                        cell_map[cell][0] + total_extensions[0][cell[0] + 1],
+                        cell_map[cell][1],
+                    )
+        cells_to_move = set([pair[1] for pair in inequalities[1]])
+        for base_cell in cells_to_move:
+            if base_cell in param.image_cells():
+                for cell in param.map.preimage_of_cell(base_cell):
+                    cell_map[cell] = (
+                        cell_map[cell][0],
+                        cell_map[cell][1] + total_extensions[1][cell[1] + 1],
+                    )
+        return cell_map
 
-        ## Expand param based on these partial sums
+    def transform_gcp(self, cell_map, gcp: GriddedCayleyPerm) -> GriddedCayleyPerm:
+        """Moves a GCP to the correct cell"""
+        new_positions = (cell_map[position] for position in gcp.positions)
+        return GriddedCayleyPerm(gcp.pattern, new_positions)
 
-    def new_row_col_map(self, partial_sum: list[int]) -> dict[int, int]:
-        """Creates a new row/col map based on the partial sums."""
+    def make_new_map(self, param: Parameter, direction, parameter_extensions):
+        """Makes the RowColMap from a new parameter to the new base tiling"""
         new_map = {}
-        last_index = 0
-        for idx, val in enumerate(partial_sum):
-            for value in range(last_index, val):
-                new_map[value] = idx
-            last_index = val
+        additions = 0
+        for item in self.preimage_map[direction].items():
+            if direction:
+                preimages = param.map.preimages_of_row(item[0])
+            else:
+                preimages = param.map.preimages_of_col(item[0])
+            to_add = parameter_extensions[direction][item[0]]
+            for i in range(len(item[1])):
+                if i:
+                    additions += to_add
+                for index in preimages:
+                    new_index = index + additions
+                    new_map[new_index] = item[1][i]
         return new_map
 
-    def separate(self) -> Iterator[MappedTiling]:
-        """Returns MappedTilings with row and column separated."""
-        new_avoiders = ParameterList(
-            [self.map_param(param) for param in self.mt.avoiding_parameters]
+    def make_new_parameter(self, base_extensions, param: Parameter) -> Parameter:
+        """Returns the adjusted param after row/col separation"""
+        param_extensions = self.parameter_extensions(base_extensions, param)
+        total_extensions = self.total_extensions(param_extensions)
+        new_dimensions = (
+            param.ghost.dimensions[0] + total_extensions[0][-1],
+            param.ghost.dimensions[1] + total_extensions[1][-1],
         )
+        cell_map = self.cell_map(total_extensions, param)
+        new_obstructions = [
+            self.transform_gcp(cell_map, ob) for ob in param.ghost.obstructions
+        ]
+        new_requirements = [
+            [self.transform_gcp(cell_map, req) for req in req_list]
+            for req_list in param.ghost.requirements
+        ]
+        new_ghost = Tiling(new_obstructions, new_requirements, new_dimensions)
+        new_map = RowColMap(
+            self.make_new_map(param, 0, param_extensions),
+            self.make_new_map(param, 1, param_extensions),
+        )
+        return Parameter(new_ghost, new_map)
+
+    def separate_base_tiling(self):
+        """Returns the row/col seperated mappling"""
+        base_extensions = self.base_extensions()
+        new_avoiders = [
+            self.make_new_parameter(base_extensions, param)
+            for param in self.avoiding_parameters
+        ]
         new_containers = [
-            ParameterList([self.map_param(param) for param in c_list])
-            for c_list in self.mt.containing_parameters
+            [self.make_new_parameter(base_extensions, param) for param in c_list]
+            for c_list in self.containing_parameters
         ]
         new_enumerators = [
-            ParameterList([self.map_param(param) for param in c_list])
-            for c_list in self.mt.enumerating_parameters
+            [self.make_new_parameter(base_extensions, param) for param in e_list]
+            for e_list in self.enumeration_parameters
         ]
-        for T in self.separation.row_col_separation():
-            yield MappedTiling(T, new_avoiders, new_containers, new_enumerators)
+        for base in self.separation.row_col_separation():
+            yield MappedTiling(base, new_avoiders, new_containers, new_enumerators)
+
+    @staticmethod
+    def separate_parameter(param: Parameter):
+        """Row/Col separates a parameter and adjusts the map without altering the base tiling"""
+        separation = LessThanOrEqualRowColSeparation(param.ghost)
+        separation_map = separation.row_col_map
+        new_col_map = {
+            i: param.map.col_map[separation_map.col_map[i]]
+            for i in separation_map.col_map
+        }
+        new_row_map = {
+            i: param.map.row_map[separation_map.row_map[i]]
+            for i in separation_map.row_map
+        }
+        for P in separation.row_col_separation():
+            yield Parameter(P, RowColMap(new_col_map, new_row_map))
+
+
+class MTRowColSeparation:
+    def __init__(self, mapped_tiling: MappedTiling, or_equal=False):
+        self.tiling = mapped_tiling.tiling
+        self.avoiding_parameters = mapped_tiling.avoiding_parameters
+        self.containing_parameters = mapped_tiling.containing_parameters
+        self.enumeration_parameters = mapped_tiling.enumeration_parameters
+        if or_equal:
+            self.separation = LessThanOrEqualRowColSeparation(self.tiling)
+        else:
+            self.separation = LessThanRowColSeparation(self.tiling)
+        self.preimage_map = self.separation.row_col_map.preimage_map()
+
+    def base_extensions(self) -> tuple:
+        """Returns how many new rows/cols are added to the new base tiling
+        for each row/col in the original tiling"""
+        row_extensions = [
+            (len(self.preimage_map[1][i]) - 1) for i in range(len(self.preimage_map[1]))
+        ]
+        col_extensions = [
+            (len(self.preimage_map[0][i]) - 1) for i in range(len(self.preimage_map[0]))
+        ]
+        return col_extensions, row_extensions
+
+    def parameter_extensions(self, base_extensions, param: Parameter):
+        """Returns how many rows/cols must be added to the new parameter
+        for each row/col in the original tiling"""
+        col_parameter_extensions = [
+            base_extensions[0][i] * len(param.map.preimages_of_col(i))
+            for i in range(len(base_extensions[0]))
+        ]
+        row_parameter_extensions = [
+            base_extensions[1][i] * len(param.map.preimages_of_row(i))
+            for i in range(len(base_extensions[1]))
+        ]
+        return col_parameter_extensions, row_parameter_extensions
+
+    def total_extensions(self, parameter_extensions):
+        """Returns how far each preimage group moves in the new parameter"""
+        total_extensions = (
+            [0]
+            + [
+                sum(parameter_extensions[0][: k + 1])
+                for k in range(len(parameter_extensions[0]))
+            ],
+            [0]
+            + [
+                sum(parameter_extensions[1][: k + 1])
+                for k in range(len(parameter_extensions[1]))
+            ],
+        )
+        return total_extensions
+
+    def cell_map_and_empty_cells(self, total_extensions, param: Parameter):
+        """Maps each cell of the original parameter to the correct active cell in the original parameter
+        also tracks which cells will be empty"""
+        cell_map = {}
+        empty_cells = set()
+        for i in range(param.ghost.dimensions[0]):
+            for j in range(param.ghost.dimensions[1]):
+                cell_map[(i, j)] = (
+                    i + total_extensions[0][param.map.col_map[i]],
+                    j + total_extensions[1][param.map.row_map[j]],
+                )
+        inequalities = self.separation.column_row_inequalities()
+        for pair in inequalities[0]:
+            for cell in param.map.preimage_of_cell(pair[1]):
+                empty_cells.add(cell_map[cell])
+                cell_map[cell] = (
+                    cell_map[cell][0] + total_extensions[0][pair[1][0] + 1],
+                    cell_map[cell][1],
+                )
+            for cell in param.map.preimage_of_cell(pair[0]):
+                empty_cells.add(
+                    (
+                        cell_map[cell][0] + total_extensions[0][pair[0][0] + 1],
+                        cell_map[cell][1],
+                    )
+                )
+        for pair in inequalities[1]:
+            for cell in param.map.preimage_of_cell(pair[1]):
+                empty_cells.add(cell_map[cell])
+                cell_map[cell] = (
+                    cell_map[cell][0],
+                    cell_map[cell][1] + total_extensions[0][pair[1][1] + 1],
+                )
+            for cell in param.map.preimage_of_cell(pair[0]):
+                empty_cells.add(
+                    (
+                        cell_map[cell][0],
+                        cell_map[1] + total_extensions[0][pair[0][1] + 1],
+                    )
+                )
+        return cell_map, empty_cells
+
+    def transform_gcp(self, cell_map, gcp: GriddedCayleyPerm) -> GriddedCayleyPerm:
+        """Moves a GCP to the correct cell"""
+        new_positions = (cell_map[position] for position in gcp.positions)
+        return GriddedCayleyPerm(gcp.pattern, new_positions)
+
+    def make_new_map(self, param: Parameter, direction, parameter_extensions):
+        """Makes the RowColMap from a new parameter to the new base tiling"""
+        new_map = {}
+        additions = 0
+        for item in self.preimage_map[direction].items():
+            if direction:
+                preimages = param.map.preimages_of_row(item[0])
+            else:
+                preimages = param.map.preimages_of_col(item[0])
+            to_add = parameter_extensions[direction][item[0]]
+            for i in range(len(item[1])):
+                if i:
+                    additions += to_add
+                for index in preimages:
+                    new_index = index + additions
+                    new_map[new_index] = item[1][i]
+        return new_map
+
+    def make_new_parameter(self, base_extensions, param: Parameter) -> Parameter:
+        """Returns the adjusted param after row/col separation"""
+        param_extensions = self.parameter_extensions(base_extensions, param)
+        total_extensions = self.total_extensions(param_extensions)
+        new_dimensions = (
+            param.ghost.dimensions[0] + total_extensions[0][-1],
+            param.ghost.dimensions[1] + total_extensions[1][-1],
+        )
+        cell_map, empty_cells = self.cell_map_and_empty_cells(total_extensions, param)
+        new_obstructions = [
+            self.transform_gcp(cell_map, ob) for ob in param.ghost.obstructions
+        ] + [
+            GriddedCayleyPerm(CayleyPermutation((0,)), (cell,)) for cell in empty_cells
+        ]
+        new_requirements = [
+            [self.transform_gcp(cell_map, req) for req in req_list]
+            for req_list in param.ghost.requirements
+        ]
+        new_ghost = Tiling(new_obstructions, new_requirements, new_dimensions)
+        new_map = RowColMap(
+            self.make_new_map(param, 0, param_extensions),
+            self.make_new_map(param, 1, param_extensions),
+        )
+        return (
+            Parameter(new_ghost, new_map)
+            .reduce_empty_rows_and_cols()
+            .reduce_by_fusion()
+        )
+
+    def separate_base_tiling(self):
+        """Returns the row/col seperated mappling"""
+        base_extensions = self.base_extensions()
+        new_avoiders = [
+            self.make_new_parameter(base_extensions, param)
+            for param in self.avoiding_parameters
+        ]
+        new_containers = [
+            [self.make_new_parameter(base_extensions, param) for param in c_list]
+            for c_list in self.containing_parameters
+        ]
+        new_enumerators = [
+            [self.make_new_parameter(base_extensions, param) for param in e_list]
+            for e_list in self.enumeration_parameters
+        ]
+        for base in self.separation.row_col_separation():
+            yield MappedTiling(base, new_avoiders, new_containers, new_enumerators)
+
+    @staticmethod
+    def separate_parameter(param: Parameter):
+        """Row/Col separates a parameter and adjusts the map without altering the base tiling"""
+        separation = LessThanOrEqualRowColSeparation(param.ghost)
+        separation_map = separation.row_col_map
+        new_col_map = {
+            i: param.map.col_map[separation_map.col_map[i]]
+            for i in separation_map.col_map
+        }
+        new_row_map = {
+            i: param.map.row_map[separation_map.row_map[i]]
+            for i in separation_map.row_map
+        }
+        for P in separation.row_col_separation():
+            yield Parameter(P, RowColMap(new_col_map, new_row_map))
--- playground/row_col_sep.py
+++ playground/row_col_sep.py
@@ -9,18 +9,26 @@ from gridded_cayley_permutations.row_col_map import RowColMap
 
 from mapplings import MappedTiling, Parameter, LTRowColSeparationMT
 
+gcp = GriddedCayleyPerm(CayleyPermutation([0, 1]), [(0, 0), (0, 2)])
+gcp2 = GriddedCayleyPerm(CayleyPermutation([0, 1]), [(0, 0), (0, 3)])
+gcp3 = GriddedCayleyPerm(CayleyPermutation([0, 1]), [(0, 1), (0, 2)])
+gcp4 = GriddedCayleyPerm(CayleyPermutation([0, 1]), [(0, 1), (0, 3)])
+# gcp1 = GriddedCayleyPerm(CayleyPermutation([0, 1]), [(0, 0), (2, 0)])
 
-tiling = Tiling([], [], (2, 2))
+tiling = Tiling([gcp, gcp2, gcp3, gcp4], [], (1, 4))
 
-mt = MappedTiling(Tiling([], [], (2, 2)), [], [], [])
+all_prs = []
+for row in range(1):
+    for col in range(2):
+        all_prs.append([GriddedCayleyPerm(CayleyPermutation([0]), [(row, col)])])
+
+# print(mt)
+
+param = Parameter(Tiling([], all_prs, (1, 2)), RowColMap({0: 0}, {0: 0, 1: 2}))
+# print(param)
+mt = MappedTiling(tiling, [param], [], [])
 
 print(mt)
-
-param = Parameter(
-    Tiling([], [], (3, 3)), RowColMap({0: 0, 1: 1, 2: 1}, {0: 0, 1: 1, 2: 1})
-)
-print(param)
-
 # print(
 #     LTRowColSeparationMT(MappedTiling(Tiling([], [], (2, 2)), [], [], [])).expansions(
 #         {0: 0, 1: 0, 2: 0, 3: 1, 4: 1}
@@ -28,4 +36,6 @@ print(param)
 # )  # should be {0: 3, 1: 2}
 
 
-print(LTRowColSeparationMT(mt).map_param(param))
+for newmt in LTRowColSeparationMT(mt).separate_base_tiling():
+    print("new one")
+    print(newmt)
