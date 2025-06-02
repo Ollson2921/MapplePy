60b4d43277fa8b42a0c8e881753df9828cd8c244
Christian Bean <christianbean93@gmail.com>
2025-06-02 11:29:06 +0100

    find packages with setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="mapped_tilings",
@@ -6,9 +6,7 @@ setup(
     description="A module for using mapped tilings.",
     author="Reed Acton, Christian Bean, and Abigail Ollson",
     author_email="c.n.bean@keele.ac.uk",
-    packages=[
-        "mapplings",
-    ],
+    packages=find_namespace_packages(),
     install_requires=[
         "comb_spec_searcher",
         "cayley_perms @ git+https://github.com/Ollson2921/CayleyPerms",
