e298c308b17334fd92f832b90c915f74d698a75e
ReedActon <110858904+ReedActon@users.noreply.github.com>
2025-08-15 00:44:30 +0000

    strategies?
    A bad reimplimentation of  the point placement factory

--- mapplings/strategies/tilescope_strategies.py
+++ mapplings/strategies/tilescope_strategies.py
@@ -13,10 +13,10 @@ from mapplings.algorithms import (
     LTORERowColSeparationMT,
     LTRowColSeparationMT,
 )
-from comb_spec_searcher import StrategyPack, DisjointUnionStrategy
+from comb_spec_searcher import StrategyPack, DisjointUnionStrategy, StrategyFactory
 from comb_spec_searcher.exception import StrategyDoesNotApply
 from gridded_cayley_permutations.point_placements import Directions
-from typing import Iterator
+from typing import Iterator, Iterable
 from cayley_permutations import CayleyPermutation
 from mapplings import MappedTiling, MTCleaner
 from comb_spec_searcher import AtomStrategy
@@ -27,15 +27,81 @@ class MapplingRequirementPlacementStrategy(RequirementPlacementStrategy):
         return MTRequirementPlacement(mappling)
 
 
-class MapplingPointPlacementFactory(PointPlacementFactory):
-    def __call__(self, comb_class: Tiling) -> Iterator[RequirementPlacementStrategy]:
+class MapplingPointPlacementFactory(StrategyFactory[MappedTiling]):
+    def __call__(self, comb_class: MappedTiling) -> Iterator[DisjointUnionStrategy]:
         for cell in comb_class.positive_cells():
+            gcps = (GriddedCayleyPerm(CayleyPermutation([0]), (cell,)),)
+            indices = (0,)
+            directionless_placement = MTRequirementPlacement(
+                comb_class
+            ).directionless_point_placement_in_cell(cell)
             for direction in Directions:
-                gcps = (GriddedCayleyPerm(CayleyPermutation([0]), (cell,)),)
-                indices = (0,)
-                yield MapplingRequirementPlacementStrategy(gcps, indices, direction)
+                yield PointPlacementFactoryStrategy(
+                    directionless_placement, gcps, indices, direction, cell
+                )
                 # if direction in PartialRequirementPlacementStrategy.DIRECTIONS:
                 #     yield PartialRequirementPlacementStrategy(gcps, indices, direction)
+
+    @classmethod
+    def from_dict(cls, d: dict) -> "MapplingPointPlacementFactory":
+        return cls(**d)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}()"
+
+    def __str__(self) -> str:
+        return "Point placement"
+
+
+class PointPlacementFactoryStrategy(
+    DisjointUnionStrategy[MappedTiling, GriddedCayleyPerm]
+):
+    def __init__(
+        self,
+        directionless_placement: MappedTiling,
+        gcps: Iterable[GriddedCayleyPerm],
+        indices: Iterable[int],
+        direction: int,
+        cell: tuple[int, int],
+        ignore_parent: bool = False,
+        possibly_empty: bool = True,
+    ):
+        self.directionless_placement = directionless_placement
+        self.gcps = tuple(gcps)
+        self.indices = tuple(indices)
+        self.direction = direction
+        self.cell = cell
+        super().__init__(ignore_parent=ignore_parent, possibly_empty=possibly_empty)
+
+    def algorithm(self, mappling: MappedTiling):
+        return MTRequirementPlacement(mappling)
+
+    def decomposition_function(self, comb_class):
+        return (comb_class.add_obstructions(self.gcps),) + (
+            self.algorithm(comb_class).force_direction(
+                self.directionless_placement,
+                self.gcps,
+                self.indices,
+                self.direction,
+                self.cell,
+            ),
+        )
+
+    def formal_step(self) -> str:
+        return (
+            f"Placed the point of the requirement {self.gcps} "
+            + f"at indices {self.indices} in direction {self.direction}"
+        )
+
+    def forward_map(self, comb_class, obj, children=None):
+        return super().forward_map(comb_class, obj, children)
+
+    def backward_map(self, comb_class, objs, children=None):
+        return super().backward_map(comb_class, objs, children)
+
+    @classmethod
+    def from_dict(cls, d):
+        raise NotImplementedError
 
 
 class CleaningStrategy(DisjointUnionStrategy[MappedTiling, GriddedCayleyPerm]):
