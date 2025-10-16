82917fb311ddf5ef724f77d4504803b725e88ffb
ReedActon <110858904+ReedActon@users.noreply.github.com>
2025-10-16 11:48:54 +0000

    Algos
    Added algorithms for IL factoring.

--- mapplings/algorithms/__init__.py
+++ mapplings/algorithms/__init__.py
@@ -1,12 +1,14 @@
 """A module for working with mapped tilings."""
 
-from .factor import Factor
+from .factor import Factor, ILFactorInverted, ILFactorNormal
 from .row_col_sep_mt import LTRowColSeparationMT, LTORERowColSeparationMT
 from .point_placement import MTRequirementPlacement
 
 
 __all__ = [
     "Factor",
+    "ILFactorNormal",
+    "ILFactorInverted"
     "LTRowColSeparationMT",
     "LTORERowColSeparationMT",
     "MTRequirementPlacement",
