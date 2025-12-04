70f77411c6a9dc5746c89753499959585b52b22e
Christian Bean <christianbean93@gmail.com>
2025-12-04 13:41:39 +0000

    Update tilescope_strategies.py

--- mapplings/strategies/tilescope_strategies.py
+++ mapplings/strategies/tilescope_strategies.py
@@ -46,12 +46,11 @@ temp = CombinatorialSpecificationSearcher.status
 def new_status(self, elaborate: bool) -> str:
     """Overwrites CSS status method"""
     return temp(self, elaborate) + MTCleaner.status_update() + ParamCleaner.status_update()
-    
 
 
-CombinatorialSpecificationSearcher.status = cast(
-    Callable[[Any, bool], str], new_status
-)  # type: ignore[method-assign,assignment]
+CombinatorialSpecificationSearcher.status = (  # type: ignore[method-assign]
+    cast(Callable[[Any, bool]], new_status)
+)
 
 
 class MapplingRequirementPlacementStrategy(RequirementPlacementStrategy):
