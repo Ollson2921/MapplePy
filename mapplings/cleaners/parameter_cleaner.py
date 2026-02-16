96c5dfecca4b44a8859c3763ce7f48f5d2162358
ReedActon <110858904+ReedActon@users.noreply.github.com>
2026-02-16 10:17:40 +0000

    Try

--- mapplings/cleaners/parameter_cleaner.py
+++ mapplings/cleaners/parameter_cleaner.py
@@ -80,7 +80,10 @@ class ParamCleaner(GenericCleaner[Parameter]):
         if not any(blank):
             return param
         col_preimages, row_preimages = param.map.preimage_map()
-        splits = param.requirement_columns_and_rows()
+        try:
+            splits = param.requirement_columns_and_rows()
+        except ValueError:
+            splits = (set[int], set[int])
 
         def to_remove(
             preimages: dict[int, tuple[int, ...]], find_rows: bool
