abe6458d6d639b2aa9e249a8989316bfce537b0d
Abigail Ollson <145692399+Ollson2921@users.noreply.github.com>
2026-03-26 11:25:25 +0000

    load 2 specs from table 1, check correct gfs

---
+++ tests/specs/test_open_specs.py
@@ -0,0 +1,16 @@
+import json
+from comb_spec_searcher import CombinatorialSpecification
+
+
+def test_open_4c_spec():
+    with open("Av(0-12)_Av(20-1)_point_place_expanded.json") as f:
+        spec = CombinatorialSpecification.from_dict(json.load(f))
+    gf = spec.get_genf()
+    assert "(x - 1)/(2*x - 1)" == str(gf)
+
+
+def test_open_4k_spec():
+    with open("Av(02-1)_Av(1-20)_row_and_col.json") as f:
+        spec = CombinatorialSpecification.from_dict(json.load(f))
+    gf = spec.get_genf()
+    assert "(x - 1)/(2*x - 1)" == str(gf)
