6f4cbc0fce77617e40aa476064659edc9ffed3c3
ReedActon <110858904+ReedActon@users.noreply.github.com>
2026-04-06 15:02:21 +0000

    New Check Reqs
    updated the test to validate reqs

--- mapplings/cleaners/unbalanced_fusion.py
+++ mapplings/cleaners/unbalanced_fusion.py
@@ -1,7 +1,6 @@
 """Algorithms and functions for uncbalanced fusion"""
 
 from typing import Iterator, Iterable
-from itertools import chain
 from gridded_cayley_permutations import Tiling, RowColMap
 from mapplings import Parameter
 
@@ -37,13 +36,9 @@ class UnbalancedFusion:
         if not indices:
             return self.param
         if fuse_rows:
-            adjust_map = RowColMap.make_adjustment_map(
-                RowColMap(*self.param_maps), [], indices, not positive_direction
-            )
+            adjust_map = self.make_adjustment_map([], indices, not positive_direction)
         else:
-            adjust_map = RowColMap.make_adjustment_map(
-                RowColMap(*self.param_maps), indices, [], not positive_direction
-            )
+            adjust_map = self.make_adjustment_map(indices, [], not positive_direction)
         mgcps = tuple(self.param.minimal_gridded_cperms())
         new_reqs = adjust_map.map_requirements((mgcps,))
         temp_param = Parameter(
@@ -95,17 +90,68 @@ class UnbalancedFusion:
         return True
 
     def check_reqs(self, index: int, fuse_rows: bool, merge_direction: int) -> bool:
-        """Makes sure requirements live entirely in one of the rows/cols we're trying to fuse"""
-        new_reqs = chain.from_iterable(
-            self.sorted_reqs[fuse_rows][index]
-            | self.sorted_reqs[fuse_rows][index + merge_direction]
-        )
-        for req in new_reqs:
-            relevant_positions = list(map(set[int], zip(*req.positions)))[fuse_rows]
-            if relevant_positions != {index} and (
-                relevant_positions
-                != {index + merge_direction}
-                # or len(self.param.map.image_rows_and_cols()[not fuse_rows]) > 1
-            ):
-                return False
+        """Validates the requirements"""
+        temp_maps = list(self.default_maps)
+        temp_maps[fuse_rows][index + merge_direction] = index
+        preimage_map = RowColMap(*temp_maps)
+        obs = self.sorted_obs[fuse_rows][index + merge_direction]
+        isolated_obs = {
+            ob: set(preimage_map.preimage_of_gridded_cperm(ob))
+            for ob in obs
+            if all(pos[fuse_rows] == index + merge_direction for pos in ob.positions)
+        }
+        combined_obs = obs | self.sorted_obs[fuse_rows][index]
+        for req_list in self.sorted_reqs[fuse_rows][index + merge_direction]:
+            for req in req_list:
+                subreq = req.sub_gridded_cayley_perm(
+                    (
+                        pos
+                        for pos in req.positions
+                        if pos[fuse_rows] == index + merge_direction
+                    )
+                )
+                for ob in isolated_obs.keys():
+                    if ob.avoids((subreq,)):
+                        if not all(
+                            pre_ob in combined_obs
+                            for pre_ob in isolated_obs[ob]
+                        ):
+                            return False
+                preimgaes = preimage_map.preimage_of_gridded_cperm(req)
+                relevant_positions = set(pos[fuse_rows] for pos in req.positions)
+                if index in relevant_positions:
+                    if not all(pre_req.avoids(obs) for pre_req in preimgaes):
+                        return False
         return True
+
+    def make_adjustment_map(
+        self,
+        deleted_cols: Iterable[int],
+        deleted_rows: Iterable[int],
+        reverse_direction: bool = False,
+    ) -> RowColMap:
+        """Makes a map from original param to that param after cols and rows are deleted"""
+        dimensions = self.param.dimensions
+        col_correction, row_correction = dict[int, int](), dict[int, int]()
+        adjust = 0
+        direction = (1, -1)[reverse_direction]
+        indices = tuple(range(dimensions[0]))
+        if reverse_direction:
+            indices = tuple(reversed(indices))
+        for i in indices:
+            if i in deleted_cols:
+                col_correction[i] = col_correction[i - direction]
+                adjust += direction
+            else:
+                col_correction[i] = i - adjust * direction
+        adjust = 0
+        indices = tuple(range(dimensions[1]))
+        if reverse_direction:
+            indices = tuple(reversed(indices))
+        for i in indices:
+            if i in deleted_rows:
+                row_correction[i] = row_correction[i - direction]
+                adjust += direction
+            else:
+                row_correction[i] = i - adjust * direction
+        return RowColMap(col_correction, row_correction)
