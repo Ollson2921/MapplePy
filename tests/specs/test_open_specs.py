e136af3bfe02504f7e507211c91578f73786cd05
Abi <a.n.ollson@keele.ac.uk>
2026-05-08 14:40:58 +0100

    wrong name

--- tests/specs/test_open_specs.py
+++ tests/specs/test_open_specs.py
@@ -9,13 +9,7 @@ def test_open_4c_spec():
     """Opens an old spec and checks still loads. Then checks can find
     the correct generating function. Also checks can find the expanded
     spec from the unexpanded version."""
-
     with open(HERE / "Av(0-12)_Av(20-1)_point_place_expanded.json") as f:
-        spec = CombinatorialSpecification.from_dict(json.load(f))
-    gf = spec.get_genf()
-    assert "(x - 1)/(2*x - 1)" == str(gf)
-
-    with open(HERE / "Av(0-12)_Av(20-1)_point_place.json") as f:
         spec = CombinatorialSpecification.from_dict(json.load(f))
     gf = spec.get_genf()
     assert "(x - 1)/(2*x - 1)" == str(gf)
