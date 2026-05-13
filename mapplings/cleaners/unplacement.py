f12a55287de84654197131e718215e11013df63b
ReedActon <110858904+ReedActon@users.noreply.github.com>
2026-05-13 13:12:21 +0000

    Various FIxes
    Import fixes and other adjustments to match parent class

--- mapplings/cleaners/unplacement.py
+++ mapplings/cleaners/unplacement.py
@@ -2,8 +2,9 @@
 
 from itertools import product
 from typing import Iterator
+from functools import cached_property
 
-from gridded_cayley_permutations import RowColMap, GriddedCayleyPermutation
+from gridded_cayley_permutations import RowColMap, GriddedCayleyPerm
 from gridded_cayley_permutations.simplify_obstructions_and_requirements import (
     SimplifyObstructionsAndRequirements as Simplify,
 )
@@ -20,17 +21,18 @@ class ParamUnplacement(PartialUnplacement):
         self.base_obs = parent_mappling.obstructions
         super().__init__(param.ghost)
 
-    def implied_point_obs(self) -> Iterator[GriddedCayleyPermutation]:
+    def implied_point_obs(self) -> Iterator[GriddedCayleyPerm]:
         """Finds point obs in the parameter that are implied by the base mappling"""
         for cells in product(self.param.empty_cells(), self.param.positive_cells()):
             pos = sorted(cells)
             pattern = (pos[0][1] > pos[1][1], pos[1][1] > pos[0][1])
-            gcp = GriddedCayleyPermutation(pattern, pos)
+            gcp = GriddedCayleyPerm(pattern, pos)
             if self.param.map.map_gridded_cperm(gcp) not in self.base_obs:
-                yield GriddedCayleyPermutation((0,), (cells[0],))
+                yield GriddedCayleyPerm((0,), (cells[0],))
 
-    def expected_obs(self) -> set[GriddedCayleyPermutation]:
-        unsimplified = super().expected_obs() | set(self.implied_point_obs())
+    @cached_property
+    def expected_obs(self) -> set[GriddedCayleyPerm]:
+        unsimplified = super().expected_obs | set(self.implied_point_obs())
         algo = Simplify(unsimplified, tuple(tuple()), self.param.dimensions)
         algo.simplify()
         return set(algo.obstructions)
@@ -39,8 +41,16 @@ class ParamUnplacement(PartialUnplacement):
         self, check_cols: set[int], check_rows: set[int]
     ) -> tuple[set[int], set[int]]:
         """Filters the input cols and rows to only include those which can be unplaced."""
-        valid_cols = check_cols & self.point_cols
-        valid_rows = check_rows & self.point_rows
+        valid_cols = {
+            col
+            for col in check_cols & self.positive_cols
+            if (0 < col < self.dimensions[0] - 1)
+        }
+        valid_rows = {
+            row
+            for row in check_rows & self.positive_rows
+            if (0 < row < self.dimensions[1] - 1)
+        }
         valid_cols = {
             col
             for col in valid_cols
@@ -59,6 +69,8 @@ class ParamUnplacement(PartialUnplacement):
         self, unplace_cols: set[int], unplace_rows: set[int]
     ) -> Parameter:
         """Unplaces rows and cols in the ghost and creates a new row col map"""
+        if not any((unplace_cols, unplace_rows)):
+            return self.param
         new_ghost = self.unplace(unplace_cols, unplace_rows)
 
         col_preimages, row_preimages = self.adjustment_map(
@@ -78,5 +90,5 @@ class ParamUnplacement(PartialUnplacement):
     def auto_unplace(self):
         """Does all valid unplacements for the tiling's point cells"""
         temp = self.param_unplace(set(), self.find_cols_and_rows()[1])
-        new_algo = ParamUnplacement(temp)
+        new_algo = ParamUnplacement(temp, self.base)
         return new_algo.param_unplace(new_algo.find_cols_and_rows()[0], set())
